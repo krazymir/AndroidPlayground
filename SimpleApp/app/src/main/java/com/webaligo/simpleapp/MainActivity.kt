8895767a4dce74ac43e05a28009a84a29a8b7967
Krasimir Kostadinov <krasimir.kostadinov@scalefocus.com>
2023-04-14 22:28:12 +0300

    Adding some listeners

--- SimpleApp/app/src/main/java/com/webaligo/simpleapp/MainActivity.kt
+++ SimpleApp/app/src/main/java/com/webaligo/simpleapp/MainActivity.kt
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var clickCount = 0
+        val helloBtn = findViewById<Button>(R.id.helloButton)
         val helloTxtView = findViewById<TextView>(R.id.helloText)
-        helloTxtView.setOnClickListener {
-            helloTxtView.text = "Oh, you clicked me!"
+
+        helloBtn.setOnClickListener {
+            helloTxtView.text = (++clickCount).toString()
+            Toast.makeText(this, "Clicked " + clickCount.toString(), Toast.LENGTH_SHORT).show()
         }
     }
 }
