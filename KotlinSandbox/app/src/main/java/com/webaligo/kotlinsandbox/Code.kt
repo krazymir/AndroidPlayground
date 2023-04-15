d0ee044d2c991659237f56de2b1e925ee0b85011
Krasimir Kostadinov <krasimir.kostadinov@scalefocus.com>
2023-04-15 21:43:19 +0300

    Added some simple code

--- KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
+++ KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
@@ -1,6 +1,7 @@
 package com.webaligo.kotlinsandbox
 
 fun main(){
+//    TODO: Should change to a var, since I would need to change the variable
     val name = "stranger"
     print("Hello " + name)
 }
