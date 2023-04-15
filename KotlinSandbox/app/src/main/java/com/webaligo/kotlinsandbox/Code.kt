f58fe42b403faf5a8dabb5d905c56890a5341c6a
Krasimir Kostadinov <krasimir.kostadinov@scalefocus.com>
2023-04-15 22:22:38 +0300

    Boolean algebra

--- KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
+++ KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
@@ -3,7 +3,11 @@ package com.webaligo.kotlinsandbox
 fun main(){
 
     // Booleans
-    val res = (5 shl 1) == 10
+    val num = 5
+    var num2 = 4
+    num2 *= 2
+    num2 += 2
+    val res = (num shl 1) == num2
     println("The result is ${res}")
 
 }
