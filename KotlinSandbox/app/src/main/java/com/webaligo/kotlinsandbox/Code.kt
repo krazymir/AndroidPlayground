c42108a6279a1af0497d3418c38fdfaac016d4fe
Krasimir Kostadinov <krasimir.kostadinov@scalefocus.com>
2023-04-16 22:30:19 +0300

    if statements

--- KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
+++ KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
@@ -2,12 +2,18 @@ package com.webaligo.kotlinsandbox
 
 fun main(){
 
-    // Booleans
+    // Booleans & if statements
     val num = 5
     var num2 = 4
     num2 *= 2
     num2 += 2
     val res = (num shl 1) == num2
-    println("The result is ${res}")
+    if((num shl 1) == num2) {
+        println("The result is ${res}")
+    }else if (false){
+        println("Something is broken!!!")
+    }else {
+        println("Something is broken!!!")
+    }
 
 }
