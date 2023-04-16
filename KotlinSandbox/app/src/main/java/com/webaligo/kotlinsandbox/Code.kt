915a0b97c5a52876242c188deac5754f9d8bc6be
Krasimir Kostadinov <krasimir.kostadinov@scalefocus.com>
2023-04-16 22:43:22 +0300

    whens

--- KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
+++ KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
@@ -7,11 +7,23 @@ fun main(){
     var num2 = 4
     num2 *= 2
     num2 += 2
+    val something: Any = 22
     val res = (num shl 1) == num2
     when (num2){
         in 1..100 -> println("All makes sense!")
         0 -> {
             println("Nothing makes sense!")
+        }
+        else -> {
+            println("Nothing makes sense!")
+        }
+    }
+
+    when (something){
+        is Int -> println("All makes sense!")
+        !is Boolean -> {
+            // Will not be executed, since it will only execute the first match
+            println("Not a boolean!")
         }
         else -> {
             println("Nothing makes sense!")
