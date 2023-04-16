35c7ebc01cc6c1deff4ca556a24745daa4fa76e7
Krasimir Kostadinov <krasimir.kostadinov@scalefocus.com>
2023-04-16 23:04:28 +0300

    Loop-the-Loop

--- KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
+++ KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
@@ -3,31 +3,39 @@ package com.webaligo.kotlinsandbox
 fun main(){
 
     // Booleans & if statements
-    val num = 5
-    var num2 = 4
-    num2 *= 2
-    num2 += 2
-    val something: Any = 22
-    val res = (num shl 1) == num2
-    when (num2){
-        in 1..100 -> println("All makes sense!")
-        0 -> {
-            println("Nothing makes sense!")
+
+    var num = 4
+
+    while(num < 17){
+
+        if(num % 3 == 0 && num % 5 == 0){
+            println("$num FizzBuzz")
+        }else if(num % 3 == 0){
+            println("$num Fizz")
+        }else if(num % 5 == 0){
+            println("$num Buzz")
         }
-        else -> {
-            println("Nothing makes sense!")
-        }
+        num++
     }
 
-    when (something){
-        is Int -> println("All makes sense!")
-        !is Boolean -> {
-            // Will not be executed, since it will only execute the first match
-            println("Not a boolean!")
-        }
-        else -> {
-            println("Nothing makes sense!")
-        }
+    num = 0
+    do{
+        println("$num")
+    }while (++num < 10)
+
+    for(i in 0..10){
+        println("For $i")
     }
 
+    for(i in 0 until 10){
+        println("For $i")
+    }
+
+    for(i in 3 downTo 1 step 2){
+        println("For $i")
+    }
+
+    for(i in 3.downTo(1).step(2)){
+        println("For $i")
+    }
 }
