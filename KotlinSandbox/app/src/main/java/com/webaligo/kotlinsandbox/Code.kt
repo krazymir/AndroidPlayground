93c394e2fe8399cabdf4bc9b29a14d63451c59bc
Krasimir Kostadinov <krasimir.kostadinov@scalefocus.com>
2023-04-17 23:11:47 +0300

    Nullables

--- KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
+++ KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
@@ -6,11 +6,12 @@ fun main(){
     var nullableStr : String? = "Yey"
     val nullableLength = nullableStr?.length
 
+    // Using the let operator
     nullableLength?.let{println("The length is $nullableLength")}
-//  The Elvis operator
+    // The Elvis operator
     val notNullableStr = nullableStr ?:"Yey, Elvis!"
     println("The string is $notNullableStr")
 
-//  Not null assertion operator - it will throw KotlinNullPointerException if the value is null
+    // Not null assertion operator - it will throw KotlinNullPointerException if the value is null
     println("The upper case string is ${nullableStr!!.uppercase()}")
 }
