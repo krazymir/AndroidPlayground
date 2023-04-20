f60bd4f59c6f165346ee91ef2a16b7e77b1047ed
Krasimir Kostadinov <krasimir.kostadinov@scalefocus.com>
2023-04-20 09:27:19 +0300

    Data classes

--- KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
+++ KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
@@ -1,46 +1,12 @@
 package com.webaligo.kotlinsandbox
 
-import android.media.MediaActionSound
-
+data class Animal(val name: String, val species: String)
 fun main(){
-    val fluffy = Animal("Fluffy", "Polar bear")
-    val bear = Animal(species = "bear")
-    val default = Animal()
-    fluffy.introduction()
-    bear.introduction()
-    default.introduction()
-    println(fluffy.name)
-    bear.sound = "Roar!!!"
-    bear.makeSound()
-}
+    val bear = Animal("Paddington", "bear")
+    val newcomer = Animal("Paddington", "bear")
+    println(bear.equals(newcomer))
 
-class Animal public constructor(name: String = "no name", species: String = "no species"){
-    // Only get
-    var name: String
-        get() {
-            return field.lowercase()
-        }
-    // Late init
-    private lateinit var species: String
-    // Only set
-    var sound: String = "no sound"
-        set(value) {
-            field = value
-        }
-    init{
-        this.name = name
-        this.species = species
-    }
-    constructor(name: String = "no name", species: String = "no species", sound: String = "no sound"):this(name,species){
-        this.sound = sound
-    }
-
-    fun introduction(){
-        println("Hi, my name is ${name?:"Nobody"} and I am of species $species!")
-    }
-
-    fun makeSound(){
-        println(this.sound)
-    }
+    val copy = newcomer.copy()
+    println("${copy.toString()}")
 }
 
