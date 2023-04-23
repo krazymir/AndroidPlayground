ccf5d21985be371303daf6e5daf61298f571905b
Krasimir Kostadinov <krasimir.kostadinov@scalefocus.com>
2023-04-23 18:42:02 +0300

    as and is

--- KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
+++ KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
@@ -1,46 +1,23 @@
 package com.webaligo.kotlinsandbox
 
-interface Livable{
-    fun makeSound(){
-        println("blop")
+import java.util.Dictionary
+
+fun main() {
+    val mixedList: List<Any> = listOf("Bear", 72.3, 26, 12.3f)
+    for (obj in mixedList) {
+        when (obj) {
+            is String -> println("here is the length of the String: ${obj.length}")
+            is Double -> println("here is a Double: $obj")
+            is List<*> -> println("here is the size of the list: ${obj.toList().size}")
+            is Int -> println("here is the remainder of dividing by 3: ${obj % 3}")
+            !is Dictionary<*, *> -> println("I don't know what it is but it is not a dictionary")
+            else -> print("Unknown type")
+        }
     }
-
-    fun move(distanceMeters: Double)
-}
-
-abstract class Animal() : Livable{
-
-    abstract var name:String
-    abstract var species:String
-    abstract var weightKilos:Double
-    fun addWeight(amount: Double){
-        weightKilos+=amount
-    }
-    constructor(name: String, species:String) : this() {
-        this.name = name
-        this.species = species
-    }
-
-    override fun move(distanceMeters: Double){
-        println("${this.name} by $distanceMeters meters")
-    }
-}
-
-class Bear(name: String, species: String, furColor:String) : Animal(name, species){
-    override var name: String = "unnamed"
-    override var species: String = "undefined"
-    override var weightKilos: Double = 0.0
-    override fun makeSound(){
-        println("Look for the bear necessities, the simple bear necessities...")
-    }
-}
-
-fun main(){
-    val bear = Bear("Baloo", "bear", "blown")
-    val newcomer = Bear("Count Dracula", "vampire", "white")
-    println(bear == newcomer)
-    bear.addWeight(5.0)
-    bear.move(3.5)
-    bear.makeSound()
+//    Not safe
+    val elem: String = mixedList[0] as String
+//    Safe
+    val elem2: String? = mixedList[1] as? String
+    println("$elem and $elem2")
 }
 
