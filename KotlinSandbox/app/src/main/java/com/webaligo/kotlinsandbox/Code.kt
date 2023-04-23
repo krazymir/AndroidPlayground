89d9ee18252d6d09c569e5e36977865706f2c198
Krasimir Kostadinov <krasimir.kostadinov@scalefocus.com>
2023-04-23 18:23:37 +0300

    Animal is now an abstract class

--- KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
+++ KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
@@ -1,34 +1,35 @@
 package com.webaligo.kotlinsandbox
 
 interface Livable{
-    var name:String
-    var species:String
-    var weightKilos:Double
     fun makeSound(){
         println("blop")
     }
 
+    fun move(distanceMeters: Double)
+}
+
+abstract class Animal() : Livable{
+
+    abstract var name:String
+    abstract var species:String
+    abstract var weightKilos:Double
     fun addWeight(amount: Double){
         weightKilos+=amount
     }
-}
-
-open class Animal() : Livable{
-
-    override var name:String = "no name"
-    override var species:String = "unidentified"
-    override var weightKilos:Double = 0.0
     constructor(name: String, species:String) : this() {
         this.name = name
         this.species = species
     }
 
-    fun move(distanceMeters: Double){
+    override fun move(distanceMeters: Double){
         println("${this.name} by $distanceMeters meters")
     }
 }
 
 class Bear(name: String, species: String, furColor:String) : Animal(name, species){
+    override var name: String = "unnamed"
+    override var species: String = "undefined"
+    override var weightKilos: Double = 0.0
     override fun makeSound(){
         println("Look for the bear necessities, the simple bear necessities...")
     }
