60542ac45585d64eb53716808462943628a35aed
Krasimir Kostadinov <krasimir.kostadinov@scalefocus.com>
2023-04-27 16:40:19 +0300

    lists

--- KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
+++ KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
@@ -1,23 +1,25 @@
 package com.webaligo.kotlinsandbox
 
-import java.util.Dictionary
-
 fun main() {
-    val mixedList: List<Any> = listOf("Bear", 72.3, 26, 12.3f)
-    for (obj in mixedList) {
-        when (obj) {
-            is String -> println("here is the length of the String: ${obj.length}")
-            is Double -> println("here is a Double: $obj")
-            is List<*> -> println("here is the size of the list: ${obj.toList().size}")
-            is Int -> println("here is the remainder of dividing by 3: ${obj % 3}")
-            !is Dictionary<*, *> -> println("I don't know what it is but it is not a dictionary")
-            else -> print("Unknown type")
-        }
-    }
+    var nums = arrayOf(1,5,6,100)
 //    Not safe
-    val elem: String = mixedList[0] as String
-//    Safe
-    val elem2: String? = mixedList[1] as? String
-    println("$elem and $elem2")
+    println(nums.contentToString())
+    for(elem in nums){
+        println("${elem.toString()},")
+    }
+    println(nums[2])
+    nums[2] = 12
+    println(nums[2])
+
+    val bg = BubbleGum("Turbo", 2.44)
+    val bg2 = BubbleGum("Orbit", 3.44)
+
+    val chewy = arrayOf(bg, bg2, bg.copy())
+    println(chewy.contentToString())
+
+
+
 }
+
+data class BubbleGum(val name: String, val price: Double)
 
