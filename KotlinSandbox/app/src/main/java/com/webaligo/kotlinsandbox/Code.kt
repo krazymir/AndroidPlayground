2d9c9a3d7348ff7f8c8b9944122f2928e6547bed
Krasimir Kostadinov <krasimir.kostadinov@scalefocus.com>
2023-04-27 16:40:51 +0300

    lists

--- KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
+++ KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
@@ -1,21 +1,23 @@
 package com.webaligo.kotlinsandbox
 
 fun main() {
-    var nums = arrayOf(1,5,6,100)
-//    Not safe
-    println(nums.contentToString())
+    var nums = listOf(1,5,6,100).toMutableList()
+
     for(elem in nums){
         println("${elem.toString()},")
     }
     println(nums[2])
-    nums[2] = 12
+    nums.add(12)
+    print(nums[2])
     println(nums[2])
 
     val bg = BubbleGum("Turbo", 2.44)
     val bg2 = BubbleGum("Orbit", 3.44)
 
-    val chewy = arrayOf(bg, bg2, bg.copy())
-    println(chewy.contentToString())
+    val chewy = mutabistOf(bg, bg2, bg.copy())
+    println(chewy)
+    chewy.removeAll
+    println(chewy)
 
 
 
