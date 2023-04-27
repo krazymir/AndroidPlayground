e1f47a7449774510411430ee12c85460717a009b
Krasimir Kostadinov <krasimir.kostadinov@scalefocus.com>
2023-04-27 17:40:08 +0300

    Cleanup

--- KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
+++ KotlinSandbox/app/src/main/java/com/webaligo/kotlinsandbox/Code.kt
@@ -1,20 +1,6 @@
 package com.webaligo.kotlinsandbox
 
 fun main() {
-    val nums = ArrayList<Double>()
-    var avg = 0.0
-    nums.add(3.54)
-    nums.add(4.66)
-    nums.add(386.34)
-    nums.add(-32.99)
-    nums.add(3322.12)
-    nums.add(2.009)
-    nums.add(-992.0)
-    println("Average is ${nums.average()}")
-    for(n in nums){
-        avg += n
-    }
-    avg /= nums.size
-    println("Manual average is ${nums.average()}")
+
 }
 
