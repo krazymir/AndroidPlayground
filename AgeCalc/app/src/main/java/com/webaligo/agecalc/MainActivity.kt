db78f8a02943d683c137268e115b29a8d8e21ad0
Krasimir Kostadinov <krasimir.kostadinov@scalefocus.com>
2023-04-27 19:17:37 +0300

    Working on the date picker

--- AgeCalc/app/src/main/java/com/webaligo/agecalc/MainActivity.kt
+++ AgeCalc/app/src/main/java/com/webaligo/agecalc/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.webaligo.agecalc
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnDatePicker: Button = findViewById<Button>(R.id.selectDateBtn)
+        btnDatePicker.setOnClickListener{
+            datePickerClicked()
+        }
+    }
+
+    private fun datePickerClicked(){
+        Toast.makeText(this, "Date picker pressed!", Toast.LENGTH_LONG).show()
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DATE)
     }
 }
