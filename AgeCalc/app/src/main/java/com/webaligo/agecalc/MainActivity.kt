19b9fc00dcb8a2bf9bde5332c55ce4cf18f7c1f9
Krasimir Kostadinov <krasimir.kostadinov@scalefocus.com>
2023-04-30 13:31:43 +0300

    Working on the date picker and displaying the text

--- AgeCalc/app/src/main/java/com/webaligo/agecalc/MainActivity.kt
+++ AgeCalc/app/src/main/java/com/webaligo/agecalc/MainActivity.kt
@@ -4,24 +4,41 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+
+    var tvSelectedDate : TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val btnDatePicker: Button = findViewById<Button>(R.id.selectDateBtn)
+
         btnDatePicker.setOnClickListener{
             datePickerClicked()
         }
     }
 
     private fun datePickerClicked(){
-        Toast.makeText(this, "Date picker pressed!", Toast.LENGTH_LONG).show()
         val calendar = Calendar.getInstance()
         val year = calendar.get(Calendar.YEAR)
         val month = calendar.get(Calendar.MONTH)
         val day = calendar.get(Calendar.DATE)
+        DatePickerDialog(
+            this,
+            { view, year, month, dayOfMonth ->
+                tvSelectedDate = findViewById<TextView>(R.id.selectedDateText)
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                tvSelectedDate?.setText("$dayOfMonth/${month + 1}/$year")
+                val date = sdf.parse("$dayOfMonth/${month + 1}/$year")
+            },
+            year,
+            month,
+            day,
+        ).show()
     }
 }
