d6d44c9d35dc2bf2d73bc6094057e54bbd285b96
Krasimir Kostadinov <krasimir.kostadinov@scalefocus.com>
2023-05-04 11:26:11 +0300

    Added clear function

--- NuCalc/app/src/main/java/com/webaligo/nucalc/MainActivity.kt
+++ NuCalc/app/src/main/java/com/webaligo/nucalc/MainActivity.kt
@@ -5,26 +5,38 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
+    private var tvDisplay : TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        tvDisplay = findViewById(R.id.tvDisplay)
         // Setting click listeners for all calculator buttons
         val bntPanel : LinearLayout = findViewById<LinearLayout>(R.id.btnPanel)
         for(row in bntPanel.children) {
-            for (bnt in (row as LinearLayout).children) {
-                bnt.setOnClickListener {
-                    onDigit(bnt as Button)
+            for (btn in (row as LinearLayout).children) {
+                when ((btn as Button).text){
+                    "CLR" -> btn.setOnClickListener {
+                        onClear(btn as Button)
+                    }
+                    else -> btn.setOnClickListener {
+                        onDigit(btn as Button)
+                    }
                 }
             }
         }
     }
 
     private fun onDigit(button: Button){
-        Toast.makeText(this, "${button.text}", Toast.LENGTH_SHORT).show()
+        tvDisplay?.append(button.text)
+    }
+
+    private fun onClear(button: Button){
+        tvDisplay?.text = ""
     }
 }
